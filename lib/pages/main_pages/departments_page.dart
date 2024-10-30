bf25e01bd7762026d4698bf832a75a61f3534f6d
user-sysc <riosjulio117@gmail.com>
2024-10-30 13:43:21 -0500

    dropdown_departamentos-municipios

--- lib/pages/main_pages/departments_page.dart
+++ lib/pages/main_pages/departments_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../controllers/location_controller.dart'; // Ruta donde tengas el controlador
+import '../../controllers/location_controller.dart';
+import '../widgets/custom_dropdown_modal.dart'; // Ruta del nuevo widget
 
 class DepartmentsPage extends StatefulWidget {
   @override
@@ -8,13 +9,137 @@ class DepartmentsPage extends StatefulWidget {
 
 class _DepartmentsPageState extends State<DepartmentsPage> {
   final LocationController _controller = LocationController();
+  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
     _controller.initLocations().then((_) {
-      setState(() {}); // Refresca la vista cuando los datos están listos
+      setState(() {
+        _isLoading = false;
+      });
     });
+  }
+
+  void _showSelectionModal({
+    required BuildContext context,
+    required String title,
+    required List<String> items,
+    required ValueChanged<String> onItemSelected,
+  }) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      builder: (context) {
+        TextEditingController searchController = TextEditingController();
+        List<String> filteredItems = List.from(items);
+
+        return StatefulBuilder(
+          builder: (context, setModalState) {
+            void _filterItems(String query) {
+              setModalState(() {
+                filteredItems = items
+                    .where((item) =>
+                        item.toLowerCase().contains(query.toLowerCase()))
+                    .toList();
+              });
+            }
+
+            return SizedBox(
+              height: MediaQuery.of(context).size.height *
+                  0.9, // Ajusta la altura del modal
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Fila para el título y botón de cerrar
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left:
+                                  8.0), // Ajusta este valor según lo necesario
+                          child: Text(
+                            title,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.close),
+                          iconSize:
+                              24.0, // Cambia el tamaño aquí. Puedes probar con 20.0, 28.0, etc.
+                          onPressed: () => Navigator.pop(context),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    TextField(
+                      controller: searchController,
+                      decoration: InputDecoration(
+                        hintText: 'Buscar $title'.toLowerCase(),
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onChanged: _filterItems,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: filteredItems.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(filteredItems[index]),
+                            onTap: () {
+                              onItemSelected(filteredItems[index]);
+                              Navigator.pop(context);
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  void _openDepartmentSelector(BuildContext context) {
+    _showSelectionModal(
+      context: context,
+      title: 'Departamento',
+      items: _controller.departments,
+      onItemSelected: (selectedDepartment) {
+        setState(() {
+          _controller.selectedDepartment = selectedDepartment;
+          _controller.updateMunicipalities(selectedDepartment);
+        });
+      },
+    );
+  }
+
+  void _openMunicipalitySelector(BuildContext context) {
+    _showSelectionModal(
+      context: context,
+      title: 'Municipio',
+      items: _controller.municipalities,
+      onItemSelected: (selectedMunicipality) {
+        setState(() {
+          _controller.selectedMunicipality = selectedMunicipality;
+        });
+      },
+    );
   }
 
   @override
@@ -23,68 +148,49 @@ class _DepartmentsPageState extends State<DepartmentsPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Departamentos y Municipios"),
+        title: Text("Agregar una nueva dirección"),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Dropdown para seleccionar el departamento
-            SizedBox(
-              width:
-                  screenWidth * 0.9, // Ajusta el ancho a un 90% de la pantalla
-              child: DropdownButton<String>(
-                value: _controller.selectedDepartment,
-                hint: Text("Selecciona un Departamento"),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _controller.selectedDepartment = newValue;
-                    _controller.updateMunicipalities(newValue!);
-                  });
-                },
-                items: _controller.departments.map((department) {
-                  return DropdownMenuItem<String>(
-                    value: department,
-                    child: Flexible(
-                      child: Text(
-                        department,
-                        overflow: TextOverflow
-                            .ellipsis, // Aplica puntos suspensivos en caso de desbordamiento
-                      ),
+        padding: const EdgeInsets.all(16.0),
+        child: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Departamento *",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: 8),
+                  GestureDetector(
+                    onTap: () => _openDepartmentSelector(context),
+                    child: CustomModalDropdownButton(
+                      hint: _controller.selectedDepartment ?? "Seleccionar",
+                      value: _controller.selectedDepartment,
+                      width: screenWidth,
                     ),
-                  );
-                }).toList(),
-              ),
-            ),
-
-            SizedBox(height: 16), // Espacio entre dropdowns
-
-            // Dropdown para seleccionar el municipio
-            SizedBox(
-              width:
-                  screenWidth * 0.9, // Ajusta el ancho a un 90% de la pantalla
-              child: DropdownButton<String>(
-                value: _controller.selectedMunicipality,
-                hint: Text("Selecciona un Municipio"),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _controller.selectedMunicipality = newValue;
-                  });
-                },
-                items: _controller.municipalities.map((municipality) {
-                  return DropdownMenuItem<String>(
-                    value: municipality,
-                    child: Text(
-                      municipality,
-                      overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 24),
+                  Text(
+                    "Municipio *",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: 8),
+                  GestureDetector(
+                    onTap: () {
+                      if (_controller.selectedDepartment != null &&
+                          _controller.municipalities.isNotEmpty) {
+                        _openMunicipalitySelector(context);
+                      }
+                    },
+                    child: CustomModalDropdownButton(
+                      hint: _controller.selectedMunicipality ?? "Seleccionar",
+                      value: _controller.selectedMunicipality,
+                      width: screenWidth,
                     ),
-                  );
-                }).toList(),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
